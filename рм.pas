cbb21a5f3ae388bf2b748aed8a4bdbed28abf493
Razvo1ution <117993907+Razvo1ution@users.noreply.github.com>
2022-11-11 16:29:51 +0300

    ٩(｡•́‿•̀｡)۶
    ٩(｡•́‿•̀｡)۶

---
+++ рм.pas
@@ -0,0 +1,15 @@
+﻿var x:real;
+var a,b,y:real;
+begin
+  readln(x);
+readln(b);
+while x<=b do
+begin
+  if x<-7 then y:= (ln(x)/ln(x)*(14/69))
+  else if x<-4 then y:=power(x,1/3)/power(x,1/3)
+  else if x<6 then y:= sqr(x)+ cos(x*2)
+  else y:= power(x,1/3)*cos(x)-power(x,1/3);
+  x:=x+0.1;
+  writeln('x=',x:2:2,' y=',y:2:2);
+  end;
+end.
